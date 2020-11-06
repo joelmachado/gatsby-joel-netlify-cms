93d08652072c30a0468782ed8edfca27b27ee1c6
joel machado <joel.machado5@gmail.com>
2020-11-06 00:32:42 -0300

    ma

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,6 +38,7 @@ const TemplateWrapper = ({ children }) => {
           href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
           color="#ff4400"
         />
+        <link rel="manifest" href="/manifest.json"></link>
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
